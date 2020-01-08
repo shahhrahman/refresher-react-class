05944afbe4b0c5c0d4636d4dce3ab323b97b2ccb
shahhrahman <shahhabibur1@gmail.com>
2020-01-08 15:17:05 -0500

    Render location details conditionally

--- src/index.js
+++ src/index.js
@@ -6,21 +6,28 @@ class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { lat: null };
+        this.state = { lat: null, errorMessage: '' };
 
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
                 this.setState({ lat: position.coords.latitude });
             },
             (err) => {
-                console.log(err);
+                this.setState({ errorMessage: err.message });
             }
         );
     }
 
     render() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
 
-        return <div>Latitude: {this.state.lat}</div>;
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>;
+        }
+
+        return <div>Loading! </div>
     }
 }
 
